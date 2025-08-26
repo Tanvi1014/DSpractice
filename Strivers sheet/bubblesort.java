6ceb28885706553b40b4d68df4ca277dd7383e21
Tanvi <contactttanvi@gmail.com>
2025-08-26 10:15:26 +0530

    day 6

---
+++ Strivers sheet/bubblesort.java
@@ -0,0 +1,24 @@
+ import java.util.*;
+ public class bubblesort{
+
+      static void bubblesort(int arr[] ){
+    for(int i = 0 ; i< arr.length - 1 ; i ++){
+        for(int j = 0 ; j< arr.length - i -1 ; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+            }
+        }
+    }
+}
+    public static void main(String args[]){
+        int arr[] = {67 , 56 , 98, 100 , 54 };
+        bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+ }   
+    
+
