1a0e8f8d08fe16f14fedc0c566f4d16e6e99e6f4
Tanvi <contactttanvi@gmail.com>
2025-08-26 11:06:39 +0530

    day 6 - 3

--- Strivers sheet/countfreqq.java
+++ Strivers sheet/countfreqq.java
@@ -5,28 +5,36 @@
 //import java.util.Map;
 import java.util.*;
 public class countfreqq {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-         Collections.sort(list);
-         System.out.println(list);
-        int numbers[];
+      public static void main(String[] args) {
+        
+     
+       // ArrayList<Integer> list = new ArrayList<>();
+        //Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+         //Collections.sort(list);
+         //System.out.println(list);
+        int number[] ={ 1 , 4, 6 , 6 , 8, 1 };
 
-         Map<Integer, Integer> frequencyMap = new HashMap<>();
+         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int number []) {
-            frequencyMap.put(number, frequencyMap.getOrDefault(number, 0) + 1);
+        for (int numbers : number ) {
+            if (map.containsKey(numbers)){
+                map.put(numbers , map.get(number)+ 1);
+            }else{
+                map.put(numbers, 1);
+            }
+            
         }
-
-        System.out.println("Number Frequencies:");
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println("Number: " + entry.getKey() + ", Frequency: " + entry.getValue());
-
-
-
-
-    }
     
+
+       // System.out.println("Numbers Frequencies:");
+        for ( int key : map.keySet()) {
+            System.out.println(key + map.get(key));
+
+
+
+
+        }
     }
-}
+    }
+
