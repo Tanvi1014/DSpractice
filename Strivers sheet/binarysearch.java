961adc8b012ced725653c6b84a9c0a5470a8e2a5
Tanvi <contactttanvi@gmail.com>
2025-08-27 10:18:02 +0530

    day 7 - 2

---
+++ Strivers sheet/binarysearch.java
@@ -0,0 +1,34 @@
+public class binarysearch {
+     
+
+
+public static void main(String[] args) {
+    int arr[] = {0, 2 , 3, 4 ,15 , 46 , 65};
+    int target = 15;
+    int ans = Binarysearch(arr, target);
+    System.out.println(ans);
+    
+}
+    static int Binarysearch(int arr[] , int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            //find mid
+            //int mid =( start + end ) / 2;
+            //might be possible that this exceed the range to int in java
+
+            int mid = start + (end - start ) / 2;
+            if(target < arr[mid]){
+                end = mid - 1;
+            }else if( target > arr[mid]){
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+            
+
+        }
+        return -1;
+    }
+}
