d1da9d9657590cc3f4af069431179f2f4c836d68
Tanvi <contactttanvi@gmail.com>
2025-08-29 13:35:11 +0530

    day 8 -6

---
+++ Strivers sheet/rotatearr.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class rotatearr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size =sc.nextInt();
+        Integer numbers[] = new Integer[size];
+
+         for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        List<Integer> list =  Arrays.asList(numbers);
+        Collections.rotate(list,1);
+        System.out.println(list);
+
+    }
+    
+}
