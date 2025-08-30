93b9665dbaef48a679c99316f32832a34f423dc2
Tanvi <contactttanvi@gmail.com>
2025-08-30 10:31:05 +0530

    day 9 - 9

---
+++ Strivers sheet/maxsale.java
@@ -0,0 +1,16 @@
+//import java.util.*;
+public class maxsale {
+    public static void main(String[] args) {
+       // Scanner sc = new Scanner(System.in);
+        //int size = sc.nextInt();
+        int numbers[] = {2 , 3 , 7 ,9 ,8};
+        //Arrays.sort(numbers);
+       
+           int lastidx = numbers.length - 1;
+            System.out.println(lastidx);
+        }
+      
+
+    }
+    
+
