8f722a92e8ab95126836b7081616bad5ebc8f100
Tanvi <contactttanvi@gmail.com>
2025-08-30 09:41:39 +0530

    day 9 - 5

---
+++ Strivers sheet/twosum.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class twosum {
+    public static void main(String[] args) {
+        Integer numbers[] ={ 2 , 3 , 4 ,5 ,7 ,8};
+        List<Integer> list = Arrays.asList(numbers);
+        Integer num1= list.get(2);
+        Integer num2 = list.get(4);
+        System.out.println(num1+ num2);
+        }
+    }
+    
+
