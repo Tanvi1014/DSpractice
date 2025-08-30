38d0412a3f7fdfe49f06b3f1123f484004d8508d
Tanvi <contactttanvi@gmail.com>
2025-08-30 11:27:33 +0530

    day 9 - 11

---
+++ Strivers sheet/length.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class length {
+    public static void main(String[] args) {
+       // Scanner sc = new Scanner(System.in);
+       // int size = sc.nextInt();
+        Integer numbers[] = {1 , 2 , 3 , 5};
+        List<Integer> list = Arrays.asList(numbers);
+        System.out.println(list.size());
+       
+    }    
+}
