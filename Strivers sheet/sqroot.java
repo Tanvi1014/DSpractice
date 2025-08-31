50467f59ee8009e45ec3b889d9779a8d62f7be0c
Tanvi <contactttanvi@gmail.com>
2025-08-31 10:03:37 +0530

    day 10 - 2

---
+++ Strivers sheet/sqroot.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sqroot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double num = i;
+        double sqroot = Math.sqrt(i);
+        System.out.println("the sqroot of the no is" + " " + sqroot);
+        sc.close();
+    }
+    
+}
