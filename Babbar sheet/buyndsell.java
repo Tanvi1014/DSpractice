05a390792461df0eccb543ae79c82e9f0e388ee6
Tanvi <contactttanvi@gmail.com>
2025-09-01 09:50:18 +0530

    day 2 - 2

---
+++ Babbar sheet/buyndsell.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class buyndsell {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+         int numbers[] = new int[size];
+         for(int i = 0; i< size; i++){
+          numbers[i] = sc.nextInt();
+         }
+         
+
+         for(int i = 0; i<numbers.length -1 ; i++){
+            if(numbers[numbers[i]] > numbers[numbers[i+1 ]] || numbers[numbers[i]] > numbers[numbers[i - 1]]){
+                System.out.println(numbers[i]);
+            }
+         }
+         sc.close();
+    }
+    
+}
