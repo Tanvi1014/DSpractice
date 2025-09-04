34487e69ae3fb6bfe84ecb23d9c2b44bfc0d9fe0
Tanvi <contactttanvi@gmail.com>
2025-09-04 12:17:37 +0530

    day 12 - 6

---
+++ Strivers sheet/revstr2.java
@@ -0,0 +1,12 @@
+//import java.util.*;
+public class revstr2 {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        String s = "my name is tanvi";
+        StringBuilder sb = new StringBuilder(s);
+        System.out.println(sb.reverse().toString());
+        //sb.reverse().toString();
+
+    }
+    
+}
