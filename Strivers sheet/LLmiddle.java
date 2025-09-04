e6d5bdbb6fb2e07af55cd222835fd9557f606295
Tanvi <contactttanvi@gmail.com>
2025-09-04 15:09:22 +0530

    day 12 - 11

---
+++ Strivers sheet/LLmiddle.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class LLmiddle {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+       list.add(1);
+       list.add(2);
+       list.add(3);
+       list.add(4);
+       list.add(5);
+       list.add(6);
+       list.add(7);
+       System.out.println(list);
+       int middleindex = list.size() /2 ;
+       int middleel = list.get(middleindex);
+       System.out.println(middleel);
+       
+
+      
+       
+        
+
+    }
+    
+}
