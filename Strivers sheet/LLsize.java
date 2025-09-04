3ebc57052d0b87d50d366e99df47e6b043b099df
Tanvi <contactttanvi@gmail.com>
2025-09-04 14:20:55 +0530

    day 12 - 9

---
+++ Strivers sheet/LLsize.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LLsize {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+        System.out.println(list.size());
+        
+    }
+    
+    
+    
+    
+}
