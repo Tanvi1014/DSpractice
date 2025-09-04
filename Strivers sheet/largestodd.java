7e95e373a70b57a9fd426474b4f8ca93586f58fe
Tanvi <contactttanvi@gmail.com>
2025-09-04 08:29:25 +0530

    day 12 - 1

--- Strivers sheet/largestodd.java
+++ Strivers sheet/largestodd.java
@@ -1,9 +1,22 @@
 import java.util.*;
 public class largestodd {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String original = sc.nextLine();
-        
+       
     }
     
+    public String Largestodd(String number){
+        Scanner sc = new Scanner(System.in);
+        for(int i = number.length() - 1 ; i <= 0 ; i--){
+             if (Character.getNumericValue(number.charAt(i)) % 2 != 0) {
+                return number.substring(0, i + 1);
+
+        }
+        
+    }
+
+           return " ";   
+    }
 }
+
+    
+
