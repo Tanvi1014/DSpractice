6891ff42b43b79dac05dd6f51629b5c0f897a8a7
Tanvi <contactttanvi@gmail.com>
2025-09-09 08:28:21 +0530

    day 14 -4

---
+++ Strivers sheet/clearithbit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class clearithbit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int mask = ~(1 << b);
+        int result = a & mask ;
+                System.out.println("Original number: " + a + " (" + Integer.toBinaryString(a) + ")");
+        System.out.println("After clearing " + b + "th bit: " + result + " (" + Integer.toBinaryString(result) + ")");
+        sc.close();
+
+    }
+    
+}
