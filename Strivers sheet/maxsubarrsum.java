f71f673277c21f09f778c86eaf41b1e9562c7859
Tanvi <contactttanvi@gmail.com>
2025-09-10 10:45:34 +0530

    day 15 -2

---
+++ Strivers sheet/maxsubarrsum.java
@@ -0,0 +1,16 @@
+public class maxsubarrsum {
+    public static void main(String[] args) {
+        int numbers[] = {1 , 2 , 3 , 4 ,5 ,6 ,7 };
+        int n = numbers.length;
+        int maxSum = Integer.MIN_VALUE;
+        for(int st = 0; st< n ; st++){
+            int currSum = 0;
+            for(int end = st; end < n ; end++){
+                currSum += numbers[end];
+             maxSum = Math.max(currSum, maxSum);
+            }
+        }
+        
+    }
+    
+}
