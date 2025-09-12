80393c8991b4a9bee8f13d4cb8484bd95450b6f2
Tanvi <contactttanvi@gmail.com>
2025-09-12 12:38:47 +0530

    new topic 1

--- Strivers sheet/binarytree.java
+++ Strivers sheet/binarytree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class binarytree {
     static class Node {
         int data;
@@ -26,6 +27,59 @@ public class binarytree {
           return newNode;
         }
     }
+    public static void preorder(Node root){
+        if(root == null){
+            return  ;
+        } 
+        System.out.println(root.data+ " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        
+        inorder(root.left);
+        System.out.println(root.data + " ");
+        inorder(root.right);
+    }
+    public static void postorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        inorder(root.right);
+        System.out.println(root.data +" ");
+    }
+    public static void levelorder(Node root){
+        if(root == null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()) {
+            Node currNode = q.remove();
+            if(currNode == null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else {
+                    q.add(null);
+                }
+            }else{
+                System.out.println(currNode.data +" ");
+                if(currNode.left != null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right != null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int node[] ={1 ,2 ,4 , -1 , -1 , 5, -1 ,-1, 3, -1 , 6, -1 , -1 };
@@ -33,6 +87,11 @@ public class binarytree {
 
         Node root =  tree.buildTree(node);
         System.out.println(root.data);
+        inorder(root);
+        postorder(root);
+        preorder(root);
+        levelorder(root);
+
 
     }
     
