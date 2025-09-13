96819e5f250ac0dc469dfb89324ace81e3ed6b84
Tanvi <contactttanvi@gmail.com>
2025-09-13 13:38:13 +0530

    q2

---
+++ Strivers sheet/leetcode questions/tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    System.out.println("Pair: " + numbers[i] + ", " + numbers[j]);
+                }
+            }
+        }
