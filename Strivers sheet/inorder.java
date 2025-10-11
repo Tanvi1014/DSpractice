0b2bcb77bbfa6303f328b7bd48df08766600bd72
Tanvi <contactttanvi@gmail.com>
2025-10-11 14:07:42 +0530

    day 1 -2

--- Strivers sheet/inorder.java
+++ Strivers sheet/inorder.java
@@ -1,4 +1,4 @@
-//import java.util.*;
+import java.util.*;
 
 public class inorder {
     static class Node {
@@ -38,14 +38,42 @@ public class inorder {
             inorder(root.right);
         }
     }
+    public static void levelorder(Node root){
+        if(root == null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if(currNode == null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else {
+                    q.add(null);
+                }
+            }else{
+                System.out.println(currNode.data+" ");
+                if(currNode.left != null){
+                    q.add(currNode.left);
+                }
+            }if (currNode.right != null){
+                q.add(currNode.right);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
         Node root = BinaryTree.buildTree(nodes);
 
-        System.out.print("Preorder Traversal: ");
+        System.out.print("Inorder Traversal: ");
         BinaryTree.inorder(root);
+        levelorder(root);
     }
 }
 
