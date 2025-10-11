4a3f111bc711f0c64af0f1f8819c7e44eebcabe6
Tanvi <contactttanvi@gmail.com>
2025-10-11 15:08:17 +0530

    day 1 - 3

---
+++ Strivers sheet/bubblesortt.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class bubblesortt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {  //arr input
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
